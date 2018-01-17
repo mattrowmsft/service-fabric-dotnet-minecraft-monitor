25338d4158e68ffe280a5744b727bb6f03603a49
Matt Trower <mattrow@ntdev.microsoft.com>
2018-01-17 10:10:57 -0800

    Initial commit.

--- MinecraftPopulationMonitor/MinecraftPopulationMonitor.cs
+++ MinecraftPopulationMonitor/MinecraftPopulationMonitor.cs
@@ -9,6 +9,10 @@ using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace MinecraftPopulationMonitor
 {
+    using System.Net.Http;
+    using Microsoft.ApplicationInsights;
+    using Newtonsoft.Json;
+
     /// <summary>
     /// An instance of this class is created for each service instance by the Service Fabric runtime.
     /// </summary>
@@ -36,16 +40,45 @@ namespace MinecraftPopulationMonitor
             // TODO: Replace the following sample code with your own logic 
             //       or remove this RunAsync override if it's not needed in your service.
 
-            long iterations = 0;
-
+            var telemetryClient = new TelemetryClient();
+            var httpClient = new HttpClient();
             while (true)
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
-                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
+                var response = await httpClient.GetAsync("https://mcapi.us/server/status?ip=104.45.229.103", cancellationToken);
+                var answerString = await response.Content.ReadAsStringAsync();
+                var answer = JsonConvert.DeserializeObject<Answer>(answerString);
+                telemetryClient.TrackMetric("Population", answer.Players.Now);
+                telemetryClient.TrackMetric("Capacity", answer.Players.Max - answer.Players.Now);
 
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
             }
+        }
+
+        public class Answer
+        {
+            public string Status { get; set; }
+            public string Online { get; set; }
+            public string Motd { get; set; }
+            public string Error { get; set; }
+            public PlayerStats Players { get; set; }
+            public ServerStats Server { get; set; }
+            public string Last_Online { get; set; }
+            public string Last_Updated {get; set; }
+            public int Duration { get; set; }
+        }
+
+        public class PlayerStats
+        {
+            public int Max { get; set; }
+            public int Now { get; set; }
+        }
+
+        public class ServerStats
+        {
+            public string Name { get; set; }
+            public int Protocol { get; set; }
         }
     }
 }
